9db2ce0209d0056800e63b77c8719be7508c3a24
Aven <Aven@ahmad-pc.home>
2016-04-07 17:17:44 -0700

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Student Schedular</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form>
+            <div>Please enter your Student ID: <input type="text" size="10"/> <input type="submit" value="Submit"></div>
+        </form>
+    </body>
+</html>
