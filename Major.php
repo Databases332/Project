d6b514a1a84abbc1ee21a49b1382ce2bf808de71
Aven <Aven@ahmad-pc.home>
2016-04-07 17:08:52 -0700

    Major class

---
+++ Major.php
@@ -0,0 +1,18 @@
+<?php
+ 
+class Major
+{
+    private $name; 
+    function __construct($majorId) {
+        
+    }
+    public function getName()
+    {
+        return $this->name;
+    }  
+    public static function addMajor($name)
+    {
+        
+    }
+    
+}
