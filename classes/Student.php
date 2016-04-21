6d353c64e7dbf363648418cc1baba0b008e9d6d1
Austin Philp <austinphilp@yahoo.com>
2016-04-21 00:18:00 -0700

    remove phony data entry

--- classes/Student.php
+++ classes/Student.php
@@ -65,40 +65,45 @@ class Student
         $result = $conn->query("SELECT ClassName, Time FROM ClassSession INNER JOIN Class ON ClassSession.ClassName = Class.Name INNER JOIN Student ON Student.StudentId = 1 WHERE ClassSession.ClassName NOT IN ( SELECT ClassSession.ClassName FROM ClassSession inner join GradeReport ON ClassSession.SessionId = GradeReport.SessionId WHERE GradeReport.StudentId = 1  ) AND ( Student.MajorId = Class.MajorId OR Class.MajorId IS NULL  ) AND ( Class.PreRequisite IS NULL OR Class.PreRequisite IN ( SELECT ClassSession.ClassName FROM ClassSession inner join GradeReport ON ClassSession.SessionId = GradeReport.SessionId inner join Student ON Student.StudentId = 1 inner join Class ON (Class.Name = ClassSession.ClassName) WHERE GradeReport.StudentId = Student.StudentId )  ) ORDER BY Time ASC;");
         $classSessions = [];
         #temp variables
-        $time1 = [39,45,33];
-        $time2 = [40,46,26,34];
-        $time3 = [41,47,27,35];
-        $time4 = [42,48,28];
-        $time5 = [37,29];
-        $time6 = [38,30];
+        #$time1 = [39,45,33];
+        #$time2 = [40,46,26,34];
+        #$time3 = [41,47,27,35];
+        #$time4 = [42,48,28];
+        #$time5 = [37,29];
+        #$time6 = [38,30];
 
+        $time1 = [];
+        $time2 = [];
+        $time3 = [];
+        $time4 = [];
+        $time5 = [];
+        $time6 = [];
         while($row = mysqli_fetch_array($result))
         {
             print_r($row);    
-        #    $classSessionId = $row[0];
-        #    $classSession = new Session($classSessionId);
-        #    switch($classSession->{$Time})
-        #    {
-        #        case 1:
-        #            array_push($time1, $classSession);
-        #            break;
-        #        case 2:
-        #            array_push($time2, $classSession);
-        #            break;
-        #        case 3:
-        #            array_push($time3, $classSession);
-        #            break;
-        #        case 4:
-        #            array_push($time4, $classSession);
-        #            break;
-        #        case 5:
-        #            array_push($time5, $classSession);
-        #            break;
-        #        case 6:
-        #            array_push($time6, $classSession);
-        #            break;
-        #    }
-        #    array_push($classSessions, $classSession);
+            $className = $row[0];
+            switch($className->{$Time})
+            {
+                case 1:
+                    array_push($time1, $className);
+                    break;
+                case 2:
+                    array_push($time2, $className);
+                    break;
+                case 3:
+                    array_push($time3, $className);
+                    break;
+                case 4:
+                    array_push($time4, $className);
+                    break;
+                case 5:
+                    array_push($time5, $className);
+                    break;
+                case 6:
+                    array_push($time6, $className);
+                    break;
+            }
+            array_push($classSessions, $classSession);
         }
         $schedules = $this->combinations (
             array(
@@ -110,7 +115,6 @@ class Student
                 $time6
             )
         );
-        #print_r($schedules)
         return $schedules;
     }
     public static function addStudent($firstName,$lastName,$major)
