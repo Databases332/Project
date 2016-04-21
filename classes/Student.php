02379059cedd17e34526fb496b3ca5d438e414e6
Austin Philp <austinphilp@yahoo.com>
2016-04-21 00:10:10 -0700

    db fixes

--- classes/Student.php
+++ classes/Student.php
@@ -60,7 +60,7 @@ class Student
         $username = $url["user"];
         $password = $url["pass"];
         $db = substr($url["path"], 1);
-        $conn = new mysqi($server, $username, $password, $db);
+        $conn = new mysqli($server, $username, $password, $db);
         #$connection = mysqli_connect("root", "password", "Scheduler", "20");
         $result = $conn->query("SELECT ClassName, Time FROM ClassSession INNER JOIN Class ON ClassSession.ClassName = Class.Name INNER JOIN Student ON Student.StudentId = 1 WHERE ClassSession.ClassName NOT IN ( SELECT ClassSession.ClassName FROM ClassSession inner join GradeReport ON ClassSession.SessionId = GradeReport.SessionId WHERE GradeReport.StudentId = 1  ) AND ( Student.MajorId = Class.MajorId OR Class.MajorId IS NULL  ) AND ( Class.PreRequisite IS NULL OR Class.PreRequisite IN ( SELECT ClassSession.ClassName FROM ClassSession inner join GradeReport ON ClassSession.SessionId = GradeReport.SessionId inner join Student ON Student.StudentId = 1 inner join Class ON (Class.Name = ClassSession.ClassName) WHERE GradeReport.StudentId = Student.StudentId )  ) ORDER BY Time ASC;");
         $classSessions = [];
